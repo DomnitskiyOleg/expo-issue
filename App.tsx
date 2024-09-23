dbeef3666868f948a945c63add836e7cddd2cb14
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-09-23 19:42:41 +0400

    commit message

--- App.tsx
+++ App.tsx
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
+import { useVideoPlayer, VideoView, VideoSource } from 'expo-video'
+import { useEffect } from 'react'
+
+const videoSource: VideoSource = {
+  uri: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+}
 
 export default function App() {
+  const player = useVideoPlayer(videoSource)
+
+  useEffect(() => {
+    const subscription = player.addListener('statusChange', console.log)
+
+    return () => {
+      subscription.remove()
+    }
+  }, [player])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contentContainer}>
+      <VideoView
+        style={styles.video}
+        player={player}
+        allowsFullscreen
+        allowsPictureInPicture
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 50,
   },
-});
+  video: {
+    width: 350,
+    height: 275,
+  },
+  controlsContainer: {
+    padding: 10,
+  },
+})
